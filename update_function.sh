2c3d2932685850e6667905d40e3da4cad9da56e4
Andre Bertuzzi <andre@portlouis.inf.br>
2020-11-16 16:33:23 -0300

    Python3.6

--- update_function.sh
+++ update_function.sh
@@ -3,7 +3,7 @@ export AWS_DEFAULT_REGION=us-west-2
 LAMBDA=tweet_clone
 
 ## UPDATE FUNCTION WITH DEPENDENCY
-cd venv/lib/python3.7/site-packages   
+cd venv/lib/python3.6/site-packages   
 zip -r9 ${OLDPWD}/function.zip . 
 cd $OLDPWD 
 zip -g function.zip post_tweet.py settings.py .env
